85634c962809a4eb903ce001ac70848c1a15e36f
Maandla <71994080+Maandla@users.noreply.github.com>
2021-01-12 22:48:13 +0200

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,63 @@
+
+ 
+ 
+ $(document).ready(function() {
+	
+	$("html").easeScroll();
+	
+});
+
+
+ 
+
+
+$(function(){
+ 
+    $(document).on( 'scroll', function(){
+ 
+    	if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+ 
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+
+
+
+
+
+$(document).ready(function() {
+	 
+	 wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       true,       // default
+                      live:         true,        // default
+                    }
+                    );
+                    wow.init();
+					
+});
+	  
+
+
+
+
+
+
+
+
